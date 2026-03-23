f34992af09d90f8b06064303238d058930ee7a9e
Marharyta Tsyshevska <s31631@pjwstk.edu.pl>
2026-03-23 22:47:35 +0100

    Add rental entity with penalty logic

---
+++ APBDlab2.1/APBDlab2.1/Models/Rental.cs
@@ -0,0 +1,36 @@
+﻿namespace APBDlab2._1.Models;
+
+public class Rental
+{
+    public User User { get; }
+    public Equipment Equipment { get; }
+    public DateTime RentDate { get; }
+    public DateTime DueDate { get; }
+    public DateTime? ReturnDate { get; private set; }
+
+    public Rental(User user, Equipment equipment, int days)
+    {
+        User = user;
+        Equipment = equipment;
+        RentDate = DateTime.Now;
+        DueDate = RentDate.AddDays(days);
+    }
+
+    public void Return()
+    {
+        ReturnDate = DateTime.Now;
+        Equipment.IsAvailable = true;
+    }
+
+    public bool IsLate()
+    {
+        return ReturnDate.HasValue && ReturnDate > DueDate;
+    }
+
+    public double CalculatePenalty()
+    {
+        if (!IsLate()) return 0;
+        int daysLate = (ReturnDate.Value - DueDate).Days;
+        return daysLate * 5;
+    }
+}
