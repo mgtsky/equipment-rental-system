36162deadcf34892271a92269e4fb99473622631
Marharyta Tsyshevska <s31631@pjwstk.edu.pl>
2026-03-23 23:02:19 +0100

    Add demo scenario in Program.cs

--- APBDlab2.1/APBDlab2.1/Program.cs
+++ APBDlab2.1/APBDlab2.1/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using APBDlab2._1.Models;
+using APBDlab2._1.Services;
+
+class Program
+{
+    static void Main()
+    {
+        var service = new RentalService();
+
+        var student = new Student("Jan", "Kowalski");
+        var laptop = new Laptop("Dell", 16, "i7");
+
+        service.RentEquipment(student, laptop, 1);
+
+        try
+        {
+            service.RentEquipment(student, laptop, 1);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        var rental = service.GetActiveRentals(student).First();
+        service.ReturnEquipment(rental);
+    }
+}
