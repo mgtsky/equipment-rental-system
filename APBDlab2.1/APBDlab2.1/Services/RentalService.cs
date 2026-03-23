df66ea52927c39ae96dca262977cb68249bf3fbc
Marharyta Tsyshevska <s31631@pjwstk.edu.pl>
2026-03-23 23:00:50 +0100

    Implement rental service with business rules

---
+++ APBDlab2.1/APBDlab2.1/Services/RentalService.cs
@@ -0,0 +1,41 @@
+﻿using APBDlab2._1.Models;
+
+namespace APBDlab2._1.Services;
+
+
+
+public class RentalService
+{
+    private List<Rental> rentals = new List<Rental>();
+
+    public void RentEquipment(User user, Equipment equipment, int days)
+    {
+        if (!equipment.IsAvailable)
+            throw new Exception("Equipment not available");
+
+        int active = rentals.Count(r => r.User == user && r.ReturnDate == null);
+
+        if (active >= user.MaxRentals())
+            throw new Exception("User exceeded limit");
+
+        var rental = new Rental(user, equipment, days);
+        rentals.Add(rental);
+        equipment.IsAvailable = false;
+    }
+
+    public void ReturnEquipment(Rental rental)
+    {
+        rental.Return();
+        Console.WriteLine($"Penalty: {rental.CalculatePenalty()}");
+    }
+
+    public List<Rental> GetActiveRentals(User user)
+    {
+        return rentals.Where(r => r.User == user && r.ReturnDate == null).ToList();
+    }
+
+    public List<Rental> GetOverdue()
+    {
+        return rentals.Where(r => r.ReturnDate == null && DateTime.Now > r.DueDate).ToList();
+    }
+}
--- APBDlab2.1/APBDlab2.1/obj/Debug/net10.0/APBDlab2.1.AssemblyInfo.cs
+++ APBDlab2.1/APBDlab2.1/obj/Debug/net10.0/APBDlab2.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APBDlab2.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6752c40410b559aa831cb366c3d70172d01a6a2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f34992af09d90f8b06064303238d058930ee7a9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("APBDlab2.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APBDlab2.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
